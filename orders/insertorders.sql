b158f0ead1048c36dafd9b0ef33fe22d0525c8da
Michael Guez <michaelg@michaelseanguez.com>
2026-05-31 22:44:39 -0400

    05312026_1044pm

--- orders/insertorders.sql
+++ orders/insertorders.sql
@@ -1,6 +1,6 @@
 INSERT INTO orders (order_amount, customer_id, order_date, order_type) VALUES
 (15.24,30003,'2024-03-01','in-store'),
-(15.48,30001,'2024-03-05,'online'),
+(15.48,30001,'2024-03-05','online'),
 (29.99,30002,'2024-03-07','in-store'),
 (8.25,30005,'2024-03-10','in-store'),
 (44.73,30004,'2024-03-12','online'),
